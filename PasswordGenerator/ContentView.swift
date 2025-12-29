782f5a42a0632d5ae4b616b4a24d968a00a59a82
Nitish M <mnitish1705@gmail.com>
2025-12-29 17:29:42 +0530

    Initial Commit

---
+++ PasswordGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PasswordGenerator
+//
+//  Created by Nitish M on 29/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PasswordGenerator/PasswordGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordGeneratorApp.swift
+//  PasswordGenerator
+//
+//  Created by Nitish M on 29/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PasswordGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
