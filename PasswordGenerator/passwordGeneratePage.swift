4435e238ab2299ba2d9efd585259f35c012510cb
Nitish M <mnitish1705@gmail.com>
2025-12-30 19:17:33 +0530

    UI Corrections

--- PasswordGenerator/passwordGeneratePage.swift
+++ PasswordGenerator/passwordGeneratePage.swift
@@ -6,11 +6,49 @@
 //
 
 import Foundation
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 struct passwordGeneratePage: View {
+    
+    @Binding var goToPage: String
+    @Binding var buttonTapped: Bool
+
     var body: some View {
-        Text("Password generate page")
+        Color.blue .ignoresSafeArea()
+        Rectangle()
+            .fill(Color.white)
+            .edgesIgnoringSafeArea(.all)
+            .frame(width: 360, height: 770)
+            .cornerRadius(20)
+
+        VStack {
+            HStack {
+                Button {
+                    goToPage = "Home"
+                    buttonTapped = false
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .padding(12)
+                        .background(Color.black.opacity(0.2))
+                        .clipShape(Circle())
+                }
+
+                Spacer()
+            }
+            .padding()
+
+            Spacer()
+
+            Text("Password Generator")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+
+            Spacer()
+        }
     }
 }
+
